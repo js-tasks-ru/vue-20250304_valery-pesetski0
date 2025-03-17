2e67c620a7dcb4cf4162859011d3a346d6df78a1
Valery Piasetski <valery.piasetski@bgaming.com>
2025-03-17 10:20:50 +0100

    weather cards

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,15 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+  computed: {
+    formattedDate() {
+      return new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })
+    },
+  },
+
+  template: 'Сегодня {{formattedDate}}',
+})
+const app = createApp(App)
+const vm = app.mount('#app')
+window.vm = vm
